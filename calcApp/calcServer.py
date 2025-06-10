8fead49a355af70dbde23cc2d3e1f6320ff05380
Aquiles <brunoaquiles@ufpr.br>
2025-06-10 11:14:59 -0300

    Feature implementada

--- calcApp/calcServer.py
+++ calcApp/calcServer.py
@@ -8,8 +8,8 @@ Usage: <n1> <op> <n2>
   = <op>: Operator:
     + : Addition
     - : Subtraction
-    * : Multiplication""".encode()
-    # / : Division""".encode()
+    * : Multiplication
+    / : Division""".encode()
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
